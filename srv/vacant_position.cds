6d03ae05ddff05b100c134ccda1b2cc775ca67b9
Palagulla Venkata Ramireddy <palagulla.ramireddy@ltimindtree.com>
2025-11-28 07:56:14 +0000

    28nov

--- srv/vacant_position.cds
+++ srv/vacant_position.cds
@@ -5,7 +5,8 @@ service VacancyService @(path:'/odata/v4/VacancyService') {
     /// Return all active Position codes filtered by IC + EmpGroup
     action GetPositionCodes(
         IC       : String,
-        EmpGroup : String
+        EmpGroup : String,
+        Button   : String
     ) returns {
         codes : array of String;
     };
--- srv/vacant_position.js
+++ srv/vacant_position.js
@@ -16,14 +16,24 @@ module.exports = cds.service.impl(async function () {
         const pageSize = 1000;
 
         console.log(`📌 GetPositionCodes(IC=${IC}, EmpGroup=${EmpGroup})`);
-        console.log(`Fetching Positions from SF...`);
+        console.log(`Fetching Positions from SF...`); 
 
-        while (true) {
-            const url =
-                `/odata/v2/Position?$skip=${skip}&$top=${pageSize}` +
-                `&$filter=businessUnit eq '${IC}' and cust_EmployeeGroup eq '${EmpGroup}' and effectiveStatus eq 'A'` +
-                `&$select=code,externalName_defaultValue,effectiveStartDate,parentPosition/code` +
-                `&$expand=parentPosition`;
+    let filter = "effectiveStatus eq 'A'";
+    if (IC && EmpGroup) {
+        filter += ` and businessUnit eq '${IC}' and cust_EmployeeGroup eq '${EmpGroup}'`;
+    } else if (IC) {
+        filter += ` and businessUnit eq '${IC}'`;
+    } else if (EmpGroup) {
+        filter += ` and cust_EmployeeGroup eq '${EmpGroup}'`;
+    }
+    // If both are empty, only filter by effectiveStatus
+
+    while (true) {
+        const url =
+            `/odata/v2/Position?$skip=${skip}&$top=${pageSize}` +
+            `&$filter=${filter}` +
+            `&$select=code,externalName_defaultValue,effectiveStartDate,businessUnit,parentPosition/code` +
+            `&$expand=parentPosition`;
 
             const res = await sf.send({ method: "GET", path: url });
             const rows = res.d?.results ?? [];
@@ -43,7 +53,8 @@ module.exports = cds.service.impl(async function () {
             code: p.code,
             externalName: p.externalName_defaultValue || "",
             startDate: p.effectiveStartDate || null,
-            parentPosition: p.parentPosition?.code || null
+            parentPosition: p.parentPosition?.code || null,
+            ic: p.businessUnit || ""
         }));
 
         console.log(`✔ Returning ${data.length} minimal-position objects`);
